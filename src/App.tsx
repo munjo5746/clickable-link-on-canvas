2301ed433e615e2abad0664d69f5a78c61c6ad9c
Edward Chung <munjo5746@gmail.com>
2021-01-08 22:03:59 -0500

    example image added

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,25 @@
 import * as React from "react";
+import { fabric } from "fabric";
 import "./styles.css";
 
 export default function App() {
-  return (
-    <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
-    </div>
-  );
+  const canvasWidth = 500;
+  const canvasHeight = 500;
+  React.useEffect(() => {
+    const canvas = new fabric.Canvas("canvas");
+    canvas.setDimensions({
+      width: canvasWidth,
+      height: canvasHeight
+    });
+
+    fabric.Image.fromURL(
+      "https://www.americommerce.com/Shared/Themes/SparkPay2015-5-15/images/10-sales-promos-banner.png",
+      (img) => {
+        img.scaleToWidth(canvasWidth);
+
+        canvas.add(img);
+      }
+    );
+  }, []);
+  return <canvas id="canvas" style={{ border: "1px solid gray" }}></canvas>;
 }
